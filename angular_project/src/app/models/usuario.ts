649dc0531257d3da68b9b14329746938ec02e33b
JuanTous <83614139+JuanTous@users.noreply.github.com>
2022-02-03 03:27:01 -0500

    Update usuario.ts

--- angular_project/src/app/models/usuario.ts
+++ angular_project/src/app/models/usuario.ts
@@ -1,13 +1,7 @@
 export class usuario {
-    /*constructor(userId:number, id:number, title:string) {
-        this.userId = userId;
-        this.id = id;
-        this.title = title;
-    }*/
     id!: number;
     nombres!: string;
     apellidos!: string;
     edad!: number;
     sexo!: string;
-
 }
